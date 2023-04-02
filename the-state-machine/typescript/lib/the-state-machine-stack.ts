5fae48c0686da8a163d5086ea453dcdadad7cbd7
thesidex <42481614+thesidex@users.noreply.github.com>
2023-04-02 02:48:52 -0600

    the-state-machine

---
+++ the-state-machine/typescript/lib/the-state-machine-stack.ts
@@ -0,0 +1,147 @@
+import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigatewayv2');
+import sfn = require('@aws-cdk/aws-stepfunctions');
+import tasks = require('@aws-cdk/aws-stepfunctions-tasks');
+import { Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
+import * as sqs from "@aws-cdk/aws-sqs";
+import * as sns from "@aws-cdk/aws-sns";
+import { SqsSubscription } from "@aws-cdk/aws-sns-subscriptions";
+import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
+
+export interface property extends cdk.StackProps {
+  lambdaRoleARN?: string;
+}
+
+export class TheStateMachineStack extends cdk.Stack {
+
+  readonly alertTopic: sns.Topic;
+  readonly alertQueue: sqs.Queue;
+
+  constructor(scope: cdk.Construct, id: string, props?: property) {
+    super(scope, id, props);
+    
+    const lambdaRole = props?.lambdaRoleARN
+      ? Role.fromRoleArn(this, "LambdaRole", props.lambdaRoleARN, {
+          mutable: false,
+        })
+      : undefined;
+    
+    this.alertTopic = new sns.Topic(this, "alertTopic");
+
+    const deadLetterQueue = new sqs.Queue(this, "dlq",{
+      queueName: "lambda-dlq"
+    });
+
+    const alertQueueTimeout = cdk.Duration.seconds(30);
+    this.alertQueue = new sqs.Queue(this, "alertQueue", {
+      visibilityTimeout: alertQueueTimeout,
+      deadLetterQueue: {
+        queue: deadLetterQueue,
+        maxReceiveCount: 3
+      } 
+    });
+    this.alertTopic.addSubscription(new SqsSubscription(this.alertQueue));
+    
+    /**
+     * Step Function Starts Here
+     */
+     
+    //The first thing we need to do is see if they are asking for pineapple on a pizza
+    let pineappleCheckLambda = new lambda.Function(this, 'pineappleCheckLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-fns'),
+      handler: 'orderPizza.handler',
+      timeout: alertQueueTimeout,
+      role: lambdaRole,
+      deadLetterQueueEnabled: true,
+      deadLetterQueue: this.alertQueue,
+      //events: [new SqsEventSource(this.alertQueue)]
+    });
+
+    // Step functions are built up of steps, we need to define our first step
+    const orderPizza = new tasks.LambdaInvoke(this, "Order Pizza Job", {
+      lambdaFunction: pineappleCheckLambda,
+      inputPath: '$.flavour',
+      resultPath: '$.pineappleAnalysis',
+      payloadResponseOnly: true
+    })
+
+    // Pizza Order failure step defined
+    const pineappleDetected = new sfn.Fail(this, 'Sorry, We Dont add Pineapple', {
+      cause: 'They asked for Pineapple',
+      error: 'Failed To Make Pizza'
+    });
+
+    // If they didnt ask for pineapple let's cook the pizza
+    const cookPizza = new sfn.Succeed(this, 'Lets make your pizza', {
+      outputPath: '$.pineappleAnalysis'
+    });
+
+    //Express Step function definition
+    const definition = sfn.Chain
+    .start(orderPizza)
+    .next(new sfn.Choice(this, 'With Pineapple?') // Logical choice added to flow
+        // Look at the "status" field
+        .when(sfn.Condition.booleanEquals('$.pineappleAnalysis.containsPineapple', true), pineappleDetected) // Fail for pineapple
+        .otherwise(cookPizza));
+
+    let stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+      definition,
+      timeout: cdk.Duration.minutes(5),
+      tracingEnabled: true,
+      stateMachineType: sfn.StateMachineType.EXPRESS
+    });
+
+    /**
+     * HTTP API Definition
+     */
+    // defines an API Gateway HTTP API resource backed by our step function
+
+    // We need to give our HTTP API permission to invoke our step function
+    const httpApiRole = new Role(this, 'HttpApiRole', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+      inlinePolicies: {
+        AllowSFNExec: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: ['states:StartSyncExecution'],
+              effect: Effect.ALLOW,
+              resources: [stateMachine.stateMachineArn]
+            })
+          ]
+        })
+      }
+    })
+
+    const api = new apigw.HttpApi(this, 'the-state-machine-api', {
+      createDefaultStage: true,
+    });
+
+    // create an AWS_PROXY integration between the HTTP API and our Step Function
+    const integ = new apigw.CfnIntegration(this, 'Integ', {
+      apiId: api.httpApiId,
+      integrationType: 'AWS_PROXY',
+      connectionType: 'INTERNET',
+      integrationSubtype: 'StepFunctions-StartSyncExecution',
+      credentialsArn: httpApiRole.roleArn,
+      requestParameters: {
+        Input: "$request.body",
+        StateMachineArn: stateMachine.stateMachineArn
+      },
+      payloadFormatVersion: '1.0',
+      timeoutInMillis: 10000,
+    });
+
+    new apigw.CfnRoute(this, 'DefaultRoute', {
+      apiId: api.httpApiId,
+      routeKey: apigw.HttpRouteKey.DEFAULT.key,
+      target: `integrations/${integ.ref}`,
+    });
+
+    // output the URL of the HTTP API
+    new cdk.CfnOutput(this, 'HTTP API Url', {
+      value: api.url ?? 'Something went wrong with the deploy'
+    });
+  }
+}
